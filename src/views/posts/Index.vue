67ae8390170be383d5a65e82af7a0d762710e897
Shinan Liao <shinanliao1@gmail.com>
2021-06-14 13:57:27 -0700

    adds moment and vue2-filters

--- src/views/posts/Index.vue
+++ src/views/posts/Index.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="posts-index">
-    <div v-for="post in posts" v-bind:key="post.id">
+    <input type="text" v-model="searchTerm" placeholder="Search" />
+    <div v-for="post in filterBy(posts, searchTerm, 'title')" v-bind:key="post.id">
       <h3>Title: {{ post.title }}</h3>
       <img :src="post.image" alt="" />
       <p>Body: {{ post.body }}</p>
       <router-link :to="`/posts/${post.id}`">More Information</router-link>
+      <p>Created {{ relativeDate(post.created_at) }}</p>
       <br />
     </div>
   </div>
@@ -12,10 +14,15 @@
 
 <script>
 import axios from "axios";
+import moment from "moment";
+import Vue2Filters from "vue2-filters";
+
 export default {
+  mixins: [Vue2Filters.mixin],
   data: function () {
     return {
-      posts: []
+      posts: [],
+      searchTerm: "",
     };
   },
   created: function () {
@@ -23,6 +30,12 @@ export default {
       console.log("Posts array", response.data);
       this.posts = response.data;
     });
+  },
+
+  methods: {
+    relativeDate: function (date) {
+      return moment(date).fromNow();
+    },
   },
 };
 </script>
